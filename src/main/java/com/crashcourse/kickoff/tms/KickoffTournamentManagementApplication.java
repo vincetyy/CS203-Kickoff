0c00a8d4c964385bc6f0191f1860816c09744740
Zane Chee <zane.chee.2023@scis.smu.edu.sg>
2024-09-17 21:40:45 +0800

    Initial commit of Spring Boot project

---
+++ src/main/java/com/crashcourse/kickoff/tms/KickoffTournamentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.crashcourse.kickoff.tms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickoffTournamentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KickoffTournamentManagementApplication.class, args);
+	}
+
+}
