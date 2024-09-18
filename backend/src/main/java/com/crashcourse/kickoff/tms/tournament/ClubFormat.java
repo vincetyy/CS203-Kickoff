4ebb8c5713265d8d2e61de8605b29f9e9b8399e6
Zane Chee <zane.chee.2023@scis.smu.edu.sg>
2024-09-18 15:59:53 +0800

    added tournament object

---
+++ backend/src/main/java/com/crashcourse/kickoff/tms/tournament/ClubFormat.java
@@ -0,0 +1,13 @@
+package com.crashcourse.kickoff.tms.tournament;
+
+/*
+ * Stores Club format -
+ * Mandatory number of players
+ * Maximum number of substitutes (KIV)
+ */
+
+public enum ClubFormat {
+    FIVE_SIDE(5),
+    SEVEN_SIDE(7),
+    ELEVEN_SIDE(11);
+}
