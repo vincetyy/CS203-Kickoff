cfe8a9dead925dbc35ac9b283b02740464dc562e
Zane Chee <zane.chee.2023@scis.smu.edu.sg>
2024-10-02 21:56:21 +0800

    create testing page for creation of tournament object

---
+++ backend/src/main/java/com/crashcourse/kickoff/tms/WebConfig.java
@@ -0,0 +1,19 @@
+package com.crashcourse.kickoff.tms;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allows CORS for all endpoints
+                .allowedOrigins("*") // Change to your frontend origin
+                .allowedMethods("GET", "POST", "PATCH", "DELETE")
+                .allowedHeaders("*");
+
+    }
+}
