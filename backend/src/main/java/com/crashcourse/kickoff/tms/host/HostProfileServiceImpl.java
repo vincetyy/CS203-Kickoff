78c072ff1dd1c85e078d95edfb492a963de08d0d
Zane Chee <zane.chee.2023@scis.smu.edu.sg>
2024-10-10 15:19:43 +0800

    fixed backend for tournament creation using authorisation header

--- backend/src/main/java/com/crashcourse/kickoff/tms/host/HostProfileServiceImpl.java
+++ backend/src/main/java/com/crashcourse/kickoff/tms/host/HostProfileServiceImpl.java
@@ -1,5 +1,7 @@
 package com.crashcourse.kickoff.tms.host;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Service;
 
 import com.crashcourse.kickoff.tms.user.dto.NewUserDTO;
@@ -7,10 +9,17 @@ import com.crashcourse.kickoff.tms.user.model.User;
 
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
-import jakarta.transaction.Transactional;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
 
 @Service("hostProfileSerice")
 public class HostProfileServiceImpl implements HostProfileService{
+
+    @Autowired
+    private HostProfileRepository hostProfileRepository;
+
     @PersistenceContext
     private EntityManager entityManager;
     private HostProfileRepository hosts;
@@ -27,5 +36,15 @@ public class HostProfileServiceImpl implements HostProfileService{
         // Set properties specific to HostProfile
         newHostProfile.setUser(newUser);
         return hosts.save(newHostProfile);
+    }
+
+    @Override
+    @Transactional (readOnly = true)
+    public Optional<HostProfile> getHostProfileByID(Long id) {
+        Optional<HostProfile> hostProfile = hostProfileRepository.findById(id);
+        /*
+         * ill handle the errors and optional next time, just open an issue vince ty
+         */
+        return hostProfile;
     }
 }
